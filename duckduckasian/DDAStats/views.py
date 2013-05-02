a15ca73a6327210753bf2ea5f45e60c725589ecf
Eric Song <regonics@gmail.com>
2013-05-02 15:48:05 -0400

    successfully parsed python dictionary in JS. created buttons in viewuserstats page

--- duckduckasian/DDAStats/views.py
+++ duckduckasian/DDAStats/views.py
@@ -16,37 +16,51 @@ def ViewUserStats(request):
     race_stats = {}
 
     for race in races:
-        race_stats[race] = GroupStat(race)
+        race_stats[race] = {}
+        race_stats[race]['race_total'] = 0
+        race_stats[race]['race_attempt_1'] = 0
+        race_stats[race]['race_attempt_2'] = 0
+        race_stats[race]['race_attempt_3'] = 0
+        race_stats[race]['race_attempt_more'] = 0
+        race_stats[race]['race_skipped'] = 0
+        race_stats[race]['race_total_time'] = 0
+        race_stats[race]['age_total'] = 0
+        race_stats[race]['age_attempt_1'] = 0
+        race_stats[race]['age_attempt_2'] = 0
+        race_stats[race]['age_attempt_3'] = 0
+        race_stats[race]['age_attempt_more'] = 0
+        race_stats[race]['age_skipped'] = 0
+        race_stats[race]['age_total_time'] = 0
         
     for race in race_stats:
         user_answers = Answer_model.objects.filter(user_answering=s_user).filter(user_photo_race=race)
         for answer in user_answers:
             if(answer.question_type==race_question): #race 
                 if(answer.answer_skipped):
-                    race_stats[race].race_skipped += 1
+                    race_stats[race]['race_skipped'] += 1
                 else:
-                    race_stats[race].race_total += 1
+                    race_stats[race]['race_total'] += 1
                     if(answer.num_attempted == 1):
-                        race_stats[race].race_attempt_1 += 1
+                        race_stats[race]['race_attempt_1'] += 1
                     elif(answer.num_attempted == 2):
-                        race_stats[race].race_attempt_2 += 1
+                        race_stats[race]['race_attempt_2'] += 1
                     elif(answer.num_attempted == 3):
-                        race_stats[race].race_attempt_3 += 1
+                        race_stats[race]['race_attempt_3'] += 1
                     else:
-                        race_stats[race].race_attempt_more += 1
+                        race_stats[race]['race_attempt_more'] += 1
             elif(answer.question_type==age_question):
                 if(answer.answer_skipped):
-                    race_stats[race].age_skipped += 1
+                    race_stats[race]['age_skipped'] += 1
                 else:
-                    race_stats[race].age_total += 1
+                    race_stats[race]['age_total'] += 1
                     if(answer.num_attempted == 1):
-                        race_stats[race].age_attempt_1 += 1
+                        race_stats[race]['age_attempt_1'] += 1
                     elif(answer.num_attempted == 2):
-                        race_stats[race].age_attempt_2 += 1
+                        race_stats[race]['age_attempt_2'] += 1
                     elif(answer.num_attempted == 3):
-                        race_stats[race].age_attempt_3 += 1
+                        race_stats[race]['age_attempt_3'] += 1
                     else:
-                        race_stats[race].age_attempt_more += 1 
+                        race_stats[race]['age_attempt_more'] += 1 
 
     js_data = simplejson.dumps(race_stats) 
     return render(request, 'ViewUserStats.html', {
